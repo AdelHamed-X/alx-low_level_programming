dc1f2c7af30c7bdc76401d2e2026f0ca15738c52
Adel Hamed <55494236+AdelHamed-X@users.noreply.github.com>
2023-06-15 05:03:41 -0700

    sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,6 +10,6 @@ printf("Size of a char: " + sizeof(char) + " byte(s)\n"
 "Size of an int: " + sizeof(int) + " byte(s)\n"
 "Size of a long int: " + sizeof(long int) + " byte(s)\n"
 "Size of a long long int: " + sizeof(long long int) + " byte(s)\n"
-"Size of a float: " + sizeof(float) + " byte(s)\n";
+"Size of a float: " + sizeof(float) + " byte(s)\n");
 return (0);
 }
