da12b9a84df61a230bcc82c5790d942e422770cf
Adel Hamed <55494236+AdelHamed-X@users.noreply.github.com>
2023-06-15 04:27:40 -0700

    hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 
 /**
  * main - prints some text
- * Return: 0 
+ * Return: 0
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
-return 0;
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
