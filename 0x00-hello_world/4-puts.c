f0132897e8b9443f110bcf48edb1c5d483468a29
Adel Hamed <55494236+AdelHamed-X@users.noreply.github.com>
2023-06-15 04:02:16 -0700

    hello pust

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,9 @@
  *main function prints some words and returns 0
  */
 int main(void)
+/*
+ * main function prints some text and returns 0
+*/
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
