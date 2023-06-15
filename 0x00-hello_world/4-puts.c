70f5bbc517c32f22a432dae613141f790e7e109a
Adel Hamed <55494236+AdelHamed-X@users.noreply.github.com>
2023-06-15 04:17:30 -0700

    all fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - prints some text
+ * Return: 0
+ */
 int main(void)
-/* main function prints some text and returns 0 */
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
