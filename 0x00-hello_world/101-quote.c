08e8d5bd1452a2286641dac4bd3f1ee9c0626feb
Adel Hamed <55494236+AdelHamed-X@users.noreply.github.com>
2023-06-15 05:58:40 -0700

    stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints to the standard error stream
+ * Return: 1
+ */
+int main(void)
+{
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (1);
+}
