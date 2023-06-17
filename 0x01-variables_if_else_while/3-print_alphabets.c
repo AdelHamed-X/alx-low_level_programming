e21ce12bd0eca9846f0618efee2112a9d3342e13
Adel Hamed <55494236+AdelHamed-X@users.noreply.github.com>
2023-06-16 18:38:24 -0700

    alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -16,9 +16,9 @@ int main(void)
 		putchar(letter);
 	}
 	for (letter = 'A'; letter <= 'Z'; letter++)
-        {
-                putchar(letter);
-        }
+	{
+		putchar(letter);
+	}
 	putchar('\n');
 	return (0);
 }
