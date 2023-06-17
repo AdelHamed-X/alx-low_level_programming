e7bc40bde1eb2e11f440b478a7394967f381de19
Adel Hamed <55494236+AdelHamed-X@users.noreply.github.com>
2023-06-16 19:24:12 -0700

    comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - prints all combination of single digit
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int letter;
+
+	for (letter = '0'; letter <= '9'; letter++)
+	{
+		putchar(letter);
+		if (letter == 9)
+			continue;
+
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
