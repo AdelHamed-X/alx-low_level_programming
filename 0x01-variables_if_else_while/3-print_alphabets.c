faeeacb40c9e4458fe6391f89f36cbfea547ad75
Adel Hamed <55494236+AdelHamed-X@users.noreply.github.com>
2023-06-16 18:36:30 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - prints the alphabet
+ * Return: always 0
+ *
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	for (letter = 'A'; letter <= 'Z'; letter++)
+        {
+                putchar(letter);
+        }
+	putchar('\n');
+	return (0);
+}
