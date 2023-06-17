a8bc16e0ee70a127c3d13d5099090bbf2169882e
Adel Hamed <55494236+AdelHamed-X@users.noreply.github.com>
2023-06-16 19:05:25 -0700

    reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - prints the alphabet in reverse
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
