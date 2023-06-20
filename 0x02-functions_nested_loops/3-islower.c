b928c72d8f7e35cd956c0b040212c5ff373caae9
Adel Hamed <55494236+AdelHamed-X@users.noreply.github.com>
2023-06-20 03:57:51 -0700

    lowercase chacker

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase characters
+ * @c: is an int.
+ * Return: 1 is lowercase or 0 is not lowercase
+ */
+int _islower(int c)
+{
+	if ('c' >= 97 && 'c' <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
