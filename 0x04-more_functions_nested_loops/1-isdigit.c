f08c51cfe89b48affaae4c2078ff74786ccd8151
Adel Hamed <55494236+AdelHamed-X@users.noreply.github.com>
2023-06-22 05:43:45 -0700

    sum

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _isdigit - checks for digits (0 through 9) 
+ * _isdigit - checks for digits (0 through 9)
  * @c: an int
  * Return: 1 if digit 0 if not
  */
 int _isdigit(int c)
 {
-    if (c >= 0 && c < 10)
-        return 1;
-    else
-        return 0;
+	if (c >= 0 && c < 10)
+		return (1);
+	else
+		return (0);
 }
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: an int, @b: an int
+ * Return: sum
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
