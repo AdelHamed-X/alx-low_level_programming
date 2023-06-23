bff06474f7b49cfe73d927d9b5b756a91c32c5ea
Adel Hamed <55494236+AdelHamed-X@users.noreply.github.com>
2023-06-22 18:19:16 -0700

    5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,9 +9,9 @@ void more_numbers(void)
 	int i;
 	int j;
 
-	for (i = '0'; i <= '9'; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = '0'; j <= '14'; j++)
+		for (j = 0; j <= 14; j++)
 		{
 			_putchar(j);
 		}
