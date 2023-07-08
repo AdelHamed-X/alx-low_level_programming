1697b090309b258b9501fb184db8d90ccc6fe8c7
Adel Hamed <55494236+AdelHamed-X@users.noreply.github.com>
2023-07-07 17:19:25 -0700

    a

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+int main(int argc, char *argv[])
+{
+	for (int i = 0; i < argc; i++)
+	{
+		printf("%s\n", *argv[i]);
+	}
+	return (0);
+}
