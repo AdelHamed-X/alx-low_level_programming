561f84356ae5f48747e17fe86b3552e4c0475c1b
Adel Hamed <55494236+AdelHamed-X@users.noreply.github.com>
2023-07-12 00:11:13 -0700

    a

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - prints the string in reverse
+ *@s: a pointer
+ */
+void rev_string(char *s)
+{
+	int i, j, len;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	len = i;
+	for (j = len - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
