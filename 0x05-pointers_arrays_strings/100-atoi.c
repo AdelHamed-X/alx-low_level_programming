85360ac8a73cd8cd5cc854f227b37f918bc93abd
Adel Hamed <55494236+AdelHamed-X@users.noreply.github.com>
2023-07-12 05:07:02 -0700

    a

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+int _atoi(char *s)
+{
+	int i, sign, len;
+
+	len = 0;
+	while (s[len] == '\0')
+	{
+		len++;
+		if (s[len] == '-')
+		{
+			sign = -1;
+		}
+	}
+	for (i = 0; i < len; i++)
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result = result * 10 + *s[i];
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (result * sign);
+}
