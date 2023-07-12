fdd50d1fdd4dd98d23ab5883e0731b5ab5db18b7
Adel Hamed <55494236+AdelHamed-X@users.noreply.github.com>
2023-07-12 00:39:51 -0700

    a

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other char
+ *@str" a pointer
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		_putchar(s[i]);
+		i++;
+	}
+}
