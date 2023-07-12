0a8736f9c3f5e902bc1beb1b733f4e767aa4974d
Adel Hamed <55494236+AdelHamed-X@users.noreply.github.com>
2023-07-12 02:31:58 -0700

    a

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- *
- *
- *
+ * _strcpy - copies 2 strings
+ *@dest: a pointer
+ *@src: a pointer
+ *Return: the pointer to the dest array
  */
 char *_strcpy(char *dest, char *src)
 {
 	int i, len;
-	
+
 	len = 0;
-	while(src[len] != '\0')
+	while (src[len] != '\0')
 	{
 		len++;
 	}
