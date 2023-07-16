72079a1ec45ed466b2cf960a25ab4ff5acb22f20
Adel Hamed <55494236+AdelHamed-X@users.noreply.github.com>
2023-07-16 13:46:16 -0700

    fixed a bug

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -18,19 +18,11 @@ char *_strcat(char *dest, char *src)
 
 	while (src[src_len] != '\0')
 	{
+		dest[dest_len] = src[src_len];
 		src_len++;
+		dest_len++;
 	}
 
-	for (i = 0; i < dest_len; i++)
-	{
-		_putchar(dest[i]);
-	}
-	_putchar(' ');
-
-	for (j = 0; j < src_len; j++)
-	{
-		_putchar(src[j]);
-	}
 	_putchar('\0');
 
 	return (dest);
