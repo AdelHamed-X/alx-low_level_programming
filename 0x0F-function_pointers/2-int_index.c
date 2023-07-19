92c03219b3e01e9e1f563e5625e2e295f3ebb5e2
Adel Hamed <55494236+AdelHamed-X@users.noreply.github.com>
2023-07-19 03:32:16 -0700

    a

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,13 +10,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int index;
 
-	if (!array || !cmp)
-		return;
-
 	for (index = 0; index < size; index++)
 	{
-		if (cmp(array[index] != 0)
+		if (cmp(array[index]) != 0)
 			return index;
 		continue;
 	}
+	return index;
 }
