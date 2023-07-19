01520ead253ce01cbb2105b84e57f178a0bb4453
Adel Hamed <55494236+AdelHamed-X@users.noreply.github.com>
2023-07-19 03:11:30 -0700

    a

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,8 +1,10 @@
 #include "function_pointers.h"
 
 /**
- *
- *
+ * array_iterator - applies the function to every element in an array
+ * @array: a pointer to an array
+ * @size: size of the array
+ * @action: a funtion pointer to be used
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
