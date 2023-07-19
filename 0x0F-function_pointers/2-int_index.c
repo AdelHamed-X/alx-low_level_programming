8581d2c7425a4ffa41b3fee7c4b7205729a9cc7d
Adel Hamed <55494236+AdelHamed-X@users.noreply.github.com>
2023-07-19 03:38:44 -0700

    a

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -5,20 +5,21 @@
  * @array: a pointer to an array
  * @size: the size of the array
  * @cmp: a pointer to the function to be used
+ * Return: the index of the required int
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int index;
 
 	if (size <= 0)
-		return -1;
+		return (-1);
 	for (index = 0; index <= size; index++)
 	{
 		if (index == size)
-			return -1;
+			return (-1);
 		if (cmp(array[index]) != 0)
-			return index;
+			return (index);
 		continue;
 	}
-	return index;
+	return (index);
 }
