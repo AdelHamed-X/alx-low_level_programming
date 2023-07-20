e726174e5b6278e4c33f930f875e2ef91a046988
Adel Hamed <55494236+AdelHamed-X@users.noreply.github.com>
2023-07-20 05:15:00 -0700

    a

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,6 +1,7 @@
 #include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
+#include <string.h>
 
 /**
  * print_strings - prints stiring passed to the function
@@ -17,13 +18,13 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		if (va_arg(args, char *) == NULL)
+		if (va_arg(args, char *))
 		{
-			printf("nil");
+			printf("%s", va_arg(args, char *));
 		}
 		else
 		{
-			printf("%s", va_arg(args, char *));
+			printf("(nil)");
 		}
 
 		if (i == (n - 1))
