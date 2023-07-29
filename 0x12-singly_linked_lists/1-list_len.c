5df2d27292f616764a69ceba163e3b64318d4d73
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-07-29 12:32:12 -0700

    Fix

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -9,10 +9,12 @@ size_t list_len(const list_t *h)
 {
 	size_t n = 0;
 
-	while (!h)
+	if (h == NULL)
+		return (-1);	
+	while (h != NULL)
 	{
 		n++;
+		h = h->next;
 	}
-	printf("-> %u elements\n", n);
 	return (n);
 }
