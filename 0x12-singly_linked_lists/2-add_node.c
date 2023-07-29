0d45569b7dd1e84851fc9f97f419fb07bd172a99
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-07-29 13:23:12 -0700

    Free Fix

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -16,8 +16,8 @@ list_t *add_node(list_t **head, const char *str)
 		new->str = strdup(str);
 		if (!new->str)
 		{
-			return (NULL);
 			free(new);
+			return (NULL);;
 		}
 		new->next = *head;
 
