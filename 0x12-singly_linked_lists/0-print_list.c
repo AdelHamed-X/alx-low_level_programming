10a66699b59696b5917a242c5b0a8281cb572f53
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-07-29 11:56:52 -0700

    Fix Betty

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,13 +1,15 @@
 #include "lists.h"
 
 /**
- *
+ * print_list - prints the contents of all the nodes
+ * @h: a pointer to the first node
+ * Return: the number of elements
  */
 size_t print_list(const list_t *h)
 {
 	size_t n = 0;
 
-	while(h != NULL)
+	while (h != NULL)
 	{
 		if (h->str == NULL)
 		{
