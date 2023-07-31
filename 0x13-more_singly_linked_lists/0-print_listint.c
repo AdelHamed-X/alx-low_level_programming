bb89fc46e071d5b38ba135770922f7ee4999bb1e
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-07-31 08:11:47 -0700

    Adding Task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints the elements of list
+ * @h: a pointer
+ * Return: the elements of list
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t n = 0;
+
+	while (h)
+	{
+		printf("%i\n", h);
+
+		h = h->next;
+		n++;
+	}
+
+	return (n);
+}
---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,14 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+typedef struct listint_s
+{
+	int n;
+	struct listint_t *next;
+} listint_t;
+
+#include <stdio.h>
+
+size_t print_listint(const listint_t *h);
+
+#endif
