0d9ecd06e890cb29a3e5fc51f08388b1c7b83198
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-08-01 16:03:24 -0700

    Adding Task 2

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - adds a new node to the beginning
+ * @head: a pointer to another pointer
+ * @n: a number
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+
+	if (!head)
+		return (NULL);
+
+	new->next = *head;
+	new->n = n;
+	*head = new;
+	
+	return (new);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -15,7 +15,9 @@ typedef struct listint_t
 } listint_t;
 
 #include <stdio.h>
-
+#include <stdlib.h>
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+
 #endif
