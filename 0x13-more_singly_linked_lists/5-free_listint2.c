83ce7ca65c74698b3ccbff28eae54277d72c9ceb
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-08-05 03:56:09 -0700

    Adding free task 2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees all the nodes in the list
+ * @head: a pointer to the head
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp = *head;
+
+	if (!(*head))
+		return;
+
+	while (tmp)
+	{
+		tmp = tmp->next;
+		free(*head);
+		(*head) = tmp;
+	}
+	(*head) = NULL;
+}
