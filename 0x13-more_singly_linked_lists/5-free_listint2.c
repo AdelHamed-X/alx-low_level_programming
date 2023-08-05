df7f6a631991e462335974e7c9a71b5ec70b64d4
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-08-05 04:13:10 -0700

    all done

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,9 +11,9 @@ void free_listint2(listint_t **head)
 	if (head == NULL)
 		return;
 
-	while (tmp)
+	while (*head)
 	{
-		tmp = tmp->next;
+		tmp = (*head)->next;
 		free(*head);
 		(*head) = tmp;
 	}
