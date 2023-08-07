145f946a10e11f90b185429b154489758da11db6
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-08-07 13:33:12 -0700

    Add Task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,6 +13,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t bytesRead;
 	ssize_t bytesWrite;
 
+	if (!filename)
+		return (0);
 	fd = open(filename, O_RDWR);
 	if (fd == -1)
 		return (0);
