04b0ef4f63c38ec8afec1fcbab4f990a484d87b2
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-09-15 00:23:19 +0300

    Adding Task 4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - adds a new node at the end
+ * of a dlistint_t list
+ *
+ * @head: head of the list
+ * @n: value of the element
+ * Return: the address of the new element
+ */
+void free_dlistint(dlistint_t *head)
+{
+    dlistint_t *temp = head;
+
+    if (temp == NULL)
+        return;
+
+    while (temp->prev)
+        temp = temp->prev;
+
+    while (temp->next)
+    {
+        temp = temp->next;
+        free(temp);
+    }
+}
