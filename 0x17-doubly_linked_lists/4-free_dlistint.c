dd3eaf1e36fe4d03862ac55faee12a087fcf26fb
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-09-14 14:31:15 -0700

    Adding Task 4

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -10,17 +10,18 @@
  */
 void free_dlistint(dlistint_t *head)
 {
-    dlistint_t *temp = head;
+	dlistint_t *temp = head;
 
-    if (temp == NULL)
-        return;
+	if (temp == NULL)
+		return;
 
-    while (temp->prev)
-        temp = temp->prev;
+	while (temp->prev)
+	temp = temp->prev;
 
-    while (temp->next)
-    {
-        temp = temp->next;
-        free(temp);
-    }
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
 }
