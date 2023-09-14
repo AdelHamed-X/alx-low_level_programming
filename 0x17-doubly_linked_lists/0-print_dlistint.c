05152649acc8971177f4309f644ebf16b6c9bfe0
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-09-14 10:13:27 -0700

    Adding Task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,17 +7,17 @@
   */
 size_t print_dlistint(const dlistint_t *h)
 {
-    const dlistint_t *temp = h;
-    int counter = 0;
+	const dlistint_t *temp = h;
+	int counter = 0;
 
-    if (temp == NULL)
-        return (0);
+	if (temp == NULL)
+		return (0);
 
-    while (temp)
-    {
-        printf("%i\n", temp->n);
-        temp = temp->next;
-        counter++;
-    }
-    return (counter);
+	while (temp)
+	{
+		printf("%i\n", temp->n);
+		temp = temp->next;
+		counter++;
+	}
+	return (counter);
 }
