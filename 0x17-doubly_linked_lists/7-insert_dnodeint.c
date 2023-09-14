422fc256fbbda8f63fd9b7b0f19b94553c0ee91c
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-09-14 16:56:12 -0700

    Adding Task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -6,33 +6,33 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    dlistint_t *tmp = *h;
-    dlistint_t *new;
-    unsigned int counter = 0;
+	dlistint_t *tmp = *h;
+	dlistint_t *new;
+	unsigned int counter = 0;
 
-    new = (dlistint_t *)malloc(sizeof(dlistint_t));
-    if (!new)
-        return (NULL);
+	new = (dlistint_t *)malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
 
-    new->n = n;
-    if (idx == 0)
-    {
-        return (add_dnodeint(h, n));
-    }
+	new->n = n;
+	if (idx == 0)
+	{
+		return (add_dnodeint(h, n));
+	}
 
-    while (tmp)
-    {
-        if (counter == idx)
-        {
-            new->prev = tmp->prev;
-            new->next = tmp;
+	while (tmp)
+	{
+		if (counter == idx)
+		{
+			new->prev = tmp->prev;
+			new->next = tmp;
 
-            tmp->prev = new;
-            new->prev->next = new;
-            return (new);
-        }
-        counter++;
-        tmp = tmp->next;
-    }
-    return (NULL);
+			tmp->prev = new;
+			new->prev->next = new;
+			return (new);
+		}
+		counter++;
+		tmp = tmp->next;
+	}
+	return (NULL);
 }
