3db708d0a9d8efeed0846be504f7e8258c32d925
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-09-15 02:30:20 +0300

    Adding Task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -22,6 +22,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
     while (tmp)
     {
+        if (tmp->next == NULL)
+        {
+            return (add_dnodeint_end(h, n))
+        }
         if (counter == idx)
         {
             new->prev = tmp->prev;
