019fa8e3c5516827410e6129f6e4b6a1428adc1a
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-09-15 06:36:19 -0700

    Adding Task 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,45 +1,48 @@
 #include "lists.h"
 
 /**
- *
- *
+ * delete_dnodeint_at_index - deletes the node at index of a list
+ * @head: head of the list
+ * @index: index of the new node
+ * Return: 1 if it succeeded, -1 if it failed
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *tmp = *head;
-    unsigned int loop_cycle = 1;
+	dlistint_t *tmp = *head;
+	unsigned int loop_cycle = 1;
 
-    if (*head == NULL)
-        return (-1);
+	if (*head == NULL)
+		return (-1);
 
-    if (index == 0)
-    {
-        *head = (*head)->next;
-        if (*head != NULL)
-            (*head)->prev = NULL;
-        free(tmp);
-        return (1);
-    }
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		free(tmp);
+		return (1);
+	}
 
-    while (tmp->prev != NULL)
-    {
-        tmp = tmp->prev;
-    }
+	while (tmp->prev != NULL)
+	{
+		tmp = tmp->prev;
+	}
+	if (tmp == NULL)
+		return (-1);
 
-    for (loop_cycle = 0; tmp != NULL && loop_cycle < index; loop_cycle++)
-        tmp = tmp->next;
+	for (loop_cycle = 0; tmp != NULL && loop_cycle < index; loop_cycle++)
+		tmp = tmp->next;
 
-    if (tmp->next == NULL)
-    {
-        tmp->prev->next = NULL;
-        free(tmp);
-        return (1);
-    }
-    else
-        tmp->next->prev = tmp->prev;
+	if (tmp->next == NULL)
+	{
+		tmp->prev->next = NULL;
+		free(tmp);
+		return (1);
+	}
+	else
+		tmp->next->prev = tmp->prev;
 
-
-    tmp->prev->next = tmp->next;
-    free(tmp);
-    return (1);
+	tmp->prev->next = tmp->next;
+	free(tmp);
+	return (1);
 }
