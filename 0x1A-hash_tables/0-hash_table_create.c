ad7b508693080048c2e9d82dd62804710733813c
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-10-07 21:33:59 +0300

    Adding Task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,11 +2,11 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *array = NULL;
+	hash_table_t *array = NULL;
 
-    array = (hash_table_t *)malloc(sizeof(hash_node_t *) * size);
-    if (array == NULL)
-        return (NULL);
+	array = (hash_table_t *)malloc(sizeof(hash_node_t *) * size);
+	if (array == NULL)
+		return (NULL);
 
-    return (array);
+	return (array);
 }
