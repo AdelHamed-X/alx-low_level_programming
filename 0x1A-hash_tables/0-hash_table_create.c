3492fe6a360bd9256df4e2099f016a67c6b736a0
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-10-07 12:50:25 -0700

    Adding Task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,5 +1,10 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_create - creates a hash table
+ * @size: Hash Table size
+ * Return: a pointer to the Hash Table
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht = NULL;
