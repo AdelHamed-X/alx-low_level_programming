77f79b2813562170ff262618e07a36ebad7f3b7b
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-10-07 12:11:32 -0700

    Adding Task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,9 +4,26 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *array = NULL;
 
-	array = (hash_table_t *)malloc(sizeof(hash_node_t *) * size);
+	array = (hash_table_t *)malloc((sizeof(hash_node_t *) * size) + 1);
 	if (array == NULL)
+	{
+		free(array);
 		return (NULL);
+	}
 
 	return (array);
+}
+
+/**
+ * main - check the code for
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+    hash_table_t *ht;
+
+    ht = hash_table_create(2048);
+    printf("%p\n", (void *)ht);
+    return (EXIT_SUCCESS);
 }
