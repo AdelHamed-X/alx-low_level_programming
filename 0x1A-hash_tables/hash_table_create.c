20963416f5980075e6eaae20b5277ddcf2d040ab
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-10-07 21:20:05 +0300

    Adding Task 0

--- 0x1A-hash_tables/hash_table_create.c
+++ 0x1A-hash_tables/hash_table_create.c
@@ -2,5 +2,11 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
+    hash_table_t *array = NULL;
 
+    array = (hash_table_t)malloc(size * hash_node_s);
+    if (array == NULL)
+        return (NULL);
+
+    return (array);
 }
