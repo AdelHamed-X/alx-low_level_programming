1af6159f2aa2830eb671d3bfbc8e2ea974be5815
AdelHamed-X <adelhamed349.ah@gmail.com>
2023-10-11 02:19:16 -0700

    Adding Final Tasks

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -31,17 +31,3 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	return (ht);
 }
-
-/**
- * main - check the code for
- *
- * Return: Always EXIT_SUCCESS.
- */
-int main(void)
-{
-    hash_table_t *ht;
-
-    ht = hash_table_create(1024);
-    printf("%p\n", (void *)ht);
-    return (EXIT_SUCCESS);
-}
